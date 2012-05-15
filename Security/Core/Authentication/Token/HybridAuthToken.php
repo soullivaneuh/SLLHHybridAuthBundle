7517e965d2e5a97c06e07d914372e9f0c46e3cec
Sullivan SENECHAL <soullivaneuh@gmail.com>
2012-05-15 10:06:11 +0200

    Add token

---
+++ Security/Core/Authentication/Token/HybridAuthToken.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace SLLH\HybridAuthBundle\Security\Core\Authentification\Token;
+
+use Symfony\Component\Security\Core\Authentication\Token\AbstractToken;
+
+/**
+ * Description of HybridAuthToken
+ *
+ * @author Sullivan SENECHAL <soullivaneuh@gmail.com>
+ */
+class HybridAuthToken extends AbstractToken
+{
+    /**
+     * @var string 
+     */
+    private $uid;
+    
+    /**
+     * @param string $uid         User social network id
+     * @param array  $roles       Roles for the token
+     */
+    public function __construct($uid, array $roles = array())
+    {
+        $this->uid = $uid;
+        parent::__construct($roles);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getCredentials()
+    {
+        return $this->uid;
+    }
+}
+
+?>
