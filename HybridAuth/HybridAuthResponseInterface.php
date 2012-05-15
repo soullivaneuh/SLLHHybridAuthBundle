cd697ca79cbda033a5039e40af05f05e4f2dab10
Sullivan SENECHAL <soullivaneuh@gmail.com>
2012-05-15 18:26:26 +0200

    Implement functions to the HybridAuthResponseInterface

--- HybridAuth/HybridAuthResponseInterface.php
+++ HybridAuth/HybridAuthResponseInterface.php
@@ -1,5 +1,10 @@
 <?php
 
+namespace SLLH\HybridAuthBundle\HybridAuth;
+
+use \Hybrid_User_Profile,
+    \Hybrid_Provider_Adapter;
+
 /**
  * HybridAuthResponseInterface
  * 
@@ -7,7 +12,40 @@
  */
 interface HybridAuthResponseInterface
 {
-
+    /**
+     * Get user social network identifier 
+     * 
+     * @return string
+     */
+    public function getIdentifier();
+    
+    /**
+     * Get user social network profile
+     * 
+     * @return Hybrid_User_Profile
+     */
+    public function getUserProfile();
+    
+    /**
+     * Get user social network contacts list 
+     * 
+     * @return array
+     */
+    public function getUserContactsList();
+    
+    /**
+     * Get user social network activity
+     * 
+     * @return array 
+     */
+    public function getUserActivity();
+    
+    /**
+     * Get the Hybrid_Provider_Adapter of the authenticated user
+     * 
+     * @return Hybrid_Provider_Adapter 
+     */
+    public function getProviderAdapter();
 }
 
 ?>
