ae643284ef068fa6ce5b227ba02ce72820014e9a
Sullivan SENECHAL <soullivaneuh@gmail.com>
2012-05-18 13:13:01 +0200

    Add empty controller

---
+++ Controller/HybridAuthController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace SLLH\HybridAuthBundle\Controller;
+
+use Symfony\Component\DependencyInjection\ContainerAware,
+    Symfony\Component\HttpFoundation\Request,
+    Symfony\Component\HttpFoundation\RedirectResponse,
+    Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken,
+    Symfony\Component\Security\Core\Exception\AuthenticationException,
+    Symfony\Component\Security\Core\SecurityContext,
+    Symfony\Component\Security\Core\User\UserInterface;
+
+/**
+ * HybridAuthController
+ *
+ * @author Sullivan SENECHAL <soullivaneuh@gmail.com>
+ */
+class HybridAuthController extends ContainerAware
+{
+    //put your code here
+}
+
+?>
