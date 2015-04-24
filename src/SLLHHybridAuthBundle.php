fa56a32e963fbb119d6c19e0767eb5633f38f3b5
Sullivan SENECHAL <soullivaneuh@gmail.com>
2015-04-24 22:34:45 +0200

    Removed old symfony <2.3 bundle code

--- src/SLLHHybridAuthBundle.php
+++ src/SLLHHybridAuthBundle.php
@@ -2,41 +2,8 @@
 
 namespace SLLH\HybridAuthBundle;
 
-use Symfony\Component\HttpKernel\Bundle\Bundle,
-    Symfony\Component\DependencyInjection\ContainerBuilder,
-    Symfony\Component\HttpKernel\Kernel;
-
-use SLLH\HybridAuthBundle\DependencyInjection\SLLHHybridAuthExtension,
-    SLLH\HybridAuthBundle\DependencyInjection\Security\Factory\HybridAuthFactory;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class SLLHHybridAuthBundle extends Bundle
 {
-    /**
-     * {@inheritDoc}
-     */
-    public function build(ContainerBuilder $container)
-    {
-        parent::build($container);
-
-        //  For 2.0 versions :
-        //     factories:
-        //             - "%kernel.root_dir%/../../../vendor/bundles/SLLH/HybridAuthBundle/Resources/config/security_factory.xml"
-        if (version_compare(Kernel::VERSION, '2.1-DEV', '>=')) {
-            $extension = $container->getExtension('security');
-            $extension->addSecurityListenerFactory(new HybridAuthFactory());
-        }
-    }
-    
-    /**
-     * {@inheritdoc}
-     */
-    public function getContainerExtension()
-    {
-        // for get sllh_hybridauth instead of sllh_hybrid_auth
-        if (null === $this->extension) {
-            return new SLLHHybridAuthExtension;
-        }
-
-        return $this->extension;
-    }
 }
